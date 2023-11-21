698da0f202e98656ca2e16fe9e2cf4808161ba5f
ParkJuHyeong <ghd5427@naver.com>
2023-11-21 17:37:03 +0900

    no message

---
+++ data02/src/배열핵심/배열의평균.java
@@ -0,0 +1,26 @@
+package ¹è¿­ÇÙ½É;
+
+public class ¹è¿­ÀÇÆò±Õ {
+
+	public static void main(String[] args) {
+		int [] numbers= {1, 2,3 ,4, 5,6,7,8,9,10};
+		Solution s = new Solution();
+		double result=s.solution(numbers);
+		s.solution(numbers);
+	}
+
+}
+
+class Solution {
+	public double solution(int[] n) {
+		double answer=0;
+		
+		int sum=0;
+		for (int x: n) {
+			sum = sum+x;
+		}
+		answer=(double)sum / n.length;
+		return answer; 
+	}
+	
+} 
--- data02/src/프로그래머스입문/옷가게할인받기.java
+++ data02/src/프로그래머스입문/옷가게할인받기.java
@@ -1,15 +1,15 @@
 package 프로그래머스입문;
 
-public class 피자나눠먹기1 {
+public class 옷가게할인받기 {
 
 	public static void main(String[] args) {
-		Solution7 s = new Solution7();
+		Solution6 s = new Solution6();
 		System.out.println(s.solution(15));
 	}
 
 }
 
-class Solution7 {
+class Solution6 {
 	public int solution(int n) {
 		int answer;
 		answer=n/6;
